723bca901e0b34264635203edfc423aac1887afe
Alex <noreply@example.com>
2025-03-01 00:32:29 +0300

    added progressbar

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import pillow_heif
 import os
+from progress.bar import IncrementalBar
 
 
 def convert_to_png(image_name, folder):
@@ -26,24 +27,44 @@ def heic_to_png(directory):
     if not os.path.exists(f'{directory}/PNG'):
         os.mkdir(f'{directory}/PNG')
 
+    count_of_files = 0
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.HEIC'):
+                count_of_files += 1
+    progress_bar = IncrementalBar('Progress', max=count_of_files)
+
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith('.HEIC'):
                 convert_to_png(file, root)
-
+                progress_bar.next()
+    progress_bar.finish()
 
 def heic_to_jpeg(directory):
     if not os.path.exists(f'{directory}/JPEG'):
         os.mkdir(f'{directory}/JPEG')
 
+    count_of_files = 0
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.HEIC'):
+                count_of_files += 1
+    progress_bar = IncrementalBar('Progress', max=count_of_files)
+
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith('.HEIC'):
                 convert_to_jpg(file, root)
+                progress_bar.next()
+    progress_bar.finish()
 
 
 def main():
-    directory = input("Enter path to folder with your photos:\n")
+    while True:
+        directory = input("Enter path to folder with your photos:\n")
+        if os.path.exists(directory):
+            break
     print("Choose the converting mode:\n1. HEIC to PNG (high quality, slower)\n2. HEIC to JPEG (low quality, faster)")
     mode = 0
 
@@ -64,6 +85,7 @@ def main():
         print("Unexpected error")
 
     print("Finished!")
+    input("\nPress enter to continue")
 
 
 if __name__ == "__main__":
