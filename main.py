a72d07d1feb19af8f7e05879822b047502930eb5
Alex <noreply@example.com>
2025-03-01 15:04:39 +0300

    added threading + qol features

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from PIL import Image
 import pillow_heif
 import os
+import threading
+import time
 from progress.bar import IncrementalBar
 
 
-def convert_to_png(image_name, folder):
+def convert_to_png(image_name, folder, progress_bar):
     pillow_heif.register_heif_opener()
     image = Image.open(f'{folder}/{image_name}')
     image_name = f'{image_name[:-5]}.png'
     image.save(os.path.join(folder, 'PNG', image_name), format('PNG'))
+    progress_bar.next()
 
 
-def convert_to_jpg(image_name, folder):
+def convert_to_jpg(image_name, folder, progress_bar):
     heif_file = pillow_heif.read_heif(f'{folder}/{image_name}')
     image = Image.frombytes(
         heif_file.mode,
@@ -21,6 +24,7 @@ def convert_to_jpg(image_name, folder):
     )
     image_name = f'{image_name[:-5]}.jpg'
     image.save(os.path.join(folder, 'JPEG', image_name), format('JPEG'))
+    progress_bar.next()
 
 
 def heic_to_png(directory):
@@ -32,13 +36,18 @@ def heic_to_png(directory):
         for file in files:
             if file.endswith('.HEIC'):
                 count_of_files += 1
+
     progress_bar = IncrementalBar('Progress', max=count_of_files)
 
+    threads = []
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith('.HEIC'):
-                convert_to_png(file, root)
-                progress_bar.next()
+                t = threading.Thread(target=convert_to_png, args=(file, root, progress_bar))
+                t.start()
+                threads.append(t)
+    for t in threads:
+        t.join()
     progress_bar.finish()
 
 def heic_to_jpeg(directory):
@@ -50,13 +59,18 @@ def heic_to_jpeg(directory):
         for file in files:
             if file.endswith('.HEIC'):
                 count_of_files += 1
+
     progress_bar = IncrementalBar('Progress', max=count_of_files)
 
+    threads = []
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith('.HEIC'):
-                convert_to_jpg(file, root)
-                progress_bar.next()
+                t = threading.Thread(target=convert_to_jpg, args=(file, root, progress_bar))
+                t.start()
+                threads.append(t)
+    for t in threads:
+        t.join()
     progress_bar.finish()
 
 
@@ -77,14 +91,18 @@ def main():
             continue
         break
 
+    t1 = time.time()
     if mode == 1:
         heic_to_png(directory)
+        print(f"Finished! The results are saved in {directory}/PNG")
     elif mode == 2:
         heic_to_jpeg(directory)
+        print(f"Finished! The results are saved in {directory}/JPEG")
     else:
         print("Unexpected error")
+    t2 = time.time()
 
-    print("Finished!")
+    print("Consumed time: %.2f seconds"%(t2 - t1))
     input("\nPress enter to continue")
 
 
